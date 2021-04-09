72c347592de30685019f5881ee946417df97d3ac
jerhat <jeremy@webnest.tech>
2021-04-09 16:44:15 +0800

    python lint

--- babibot.py
+++ babibot.py
@@ -2,19 +2,23 @@ import logging
 import uci
 import sys
 
+
 def main():
 
-    logging.basicConfig(filename='logs.txt', level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='logs.txt', level=logging.INFO,
+                        format='%(asctime)s %(message)s')
+
     logging.info('Babibot started')
 
     try:
 
         while True:
             msg = input()
-            uci.commandReceived(msg)
-            
+            uci.command_received(msg)
+
     except Exception:
         logging.exception('Fatal error in main loop')
-   
+
+
 if __name__ == '__main__':
     main()
